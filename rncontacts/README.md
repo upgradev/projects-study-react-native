6fc6cbcbd74372730c47e53bd940a526e566b457
Cleison França Amorim <>
2021-05-12 22:38:35 -0400

    study

--- rncontacts/README.md
+++
@@ -1,1 +0,0 @@
-[Click to watch youtube video tutorial for this project](https://www.youtube.com/watch?v=npe3Wf4tpSg)
--- rncontacts/src/navigations/SideMenu/RootNavigator.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const navigationRef = React.createRef(null);
-
-export const navigate = (name, params) => {
-  if (navigationRef.current) {
-    navigationRef.current.navigate(name, params);
-  }
-};
