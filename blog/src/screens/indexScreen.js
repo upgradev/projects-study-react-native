9ad17bc4fa51aecd6c5e75a8a381b81545813038
Cleison França Amorim <>
2021-06-21 23:40:51 -0400

    app blog

---
+++ blog/src/screens/indexScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const indexScreen = () => {
+    return (
+        <View>
+            <Text>Index screen</Text>
+        </View>
+    )
+}
+
+export default indexScreen
+
+const styles = StyleSheet.create({})
